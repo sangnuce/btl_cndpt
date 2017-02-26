428909478114e8ea18e15f4c6089ba7dc0b95d47
Luong Van Sang <sanga2k50@gmail.com>
2017-02-26 16:18:48 +0700

    Init

---
+++ js/data.js
@@ -0,0 +1,137 @@
+var data = {
+  "products": [
+    {
+      "id": 1,
+      "name": "GÀ GIÒN GIÒN",
+      "price": 22000,
+      "unit": "miếng",
+      "is_hot_product": true
+    },
+    {
+      "id": 2,
+      "name": "ĐÙI GÀ CHICKY",
+      "price": 19000,
+      "unit": "miếng",
+      "is_hot_product": true
+    },
+    {
+      "id": 3,
+      "name": "GÀ XÙ VÀNG",
+      "price": 20000,
+      "unit": "miếng",
+      "is_hot_product": false
+    },
+    {
+      "id": 4,
+      "name": "CÁNH TEEN TEEN",
+      "price": 19000,
+      "unit": "miếng",
+      "is_hot_product": true
+    },
+    {
+      "id": 5,
+      "name": "GÀ TRUYỀN THỐNG",
+      "price": 32000,
+      "unit": "miếng",
+      "is_hot_product": false
+    },
+    {
+      "id": 6,
+      "name": "GÀ GIÒN CAY",
+      "price": 22000,
+      "unit": "miếng",
+      "is_hot_product": false
+    },
+    {
+      "id": 7,
+      "name": "CHÂN GÀ CHIÊN GIÒN",
+      "price": 7000,
+      "unit": "cặp",
+      "is_hot_product": false
+    },
+    {
+      "id": 8,
+      "name": "GÀ HOÀNG ĐẾ",
+      "price": 45000,
+      "unit": "nửa con",
+      "is_hot_product": false
+    },
+    {
+      "id": 9,
+      "name": "GÀ ZÔ ZÔ",
+      "price": 16000,
+      "unit": "miếng",
+      "is_hot_product": false
+    },
+    {
+      "id": 10,
+      "name": "KHOAI TÂY LẮC",
+      "price": 15000,
+      "unit": "phần",
+      "is_hot_product": false
+    },
+    {
+      "id": 11,
+      "name": "XÚC XÍCH SUMO",
+      "price": 10000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 12,
+      "name": "CÁ VIÊN",
+      "price": 7000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 13,
+      "name": "XÚC XÍCH PHÔ MAI",
+      "price": 7000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 14,
+      "name": "TÀU HỦ CÁ",
+      "price": 7000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 15,
+      "name": "BÒ VIÊN",
+      "price": 7000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 16,
+      "name": "CHẢ GIÒ XIÊN QUE",
+      "price": 15000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 17,
+      "name": "NUGGET CÁ",
+      "price": 12000,
+      "unit": "phần",
+      "is_hot_product": true
+    },
+    {
+      "id": 18,
+      "name": "SAO QUE",
+      "price": 12000,
+      "unit": "que",
+      "is_hot_product": false
+    },
+    {
+      "id": 19,
+      "name": "XÚC XÍCH MINI SUMO",
+      "price": 10000,
+      "unit": "que",
+      "is_hot_product": false
+    }
+  ]
+};
